9fe549c5d3a73ae3540ef02f619d47fab923fd63
Ali Raza Yusufzai <114583866+AliYusufzai@users.noreply.github.com>
2023-06-11 14:30:10 +0500

    Create Main.js

---
+++ src/components/Main.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="w-[1728px] h-[1117px] absolute top-0 left-0 bg-[#D9D9D9]">
+      <p className="w-[422px] h-[43px] absolute top-[75.71px] left-[394px] font-[600] text-[28.5557px] leading-[43px] tracking-[-0.02em] text-[#72AA83]">
+        You currently have 3 requests
+      </p>
+      <div className="w-[1114px] h-[226px] absolute top-[159px] left-[402px] border-box bg-[#FFFFFF] border-white shadow-[2px_2px_8px_rgba(0, 0, 0, 0.25)]">
+        <h1 className="w-[217px] h-[35px] absolute top-[60px] left-[40px] font-[600] text-[29.3972px] leading-[35px] tracking-[-0.08em] text-[#48AF78]">
+          St Judes Hospital
+        </h1>
+        <p className="w-[142px] h-[20px] absolute top-[100px] left-[40px] font-[500] text-[16.9257px] leading-[20px] tracking-[-0.05em] text-[#959595]">
+          Sarasota,FL. 33178
+        </p>
+        <hr className="w-[55.41px] h-[251.84px] absolute top-[-35px] left-[130px] border-black/[.3] rotate-90"></hr>
+        <h1 className="w-[131px] h-[34px] absolute top-[60px] left-[350px] font-[700] text-[27.9429px] leading-[29px] tracking-[0.20em] text-[#48AF78]">
+          10 - 17
+        </h1>
+        <p className="w-[61px] h-[20px] absolute top-[100px] left-[355px] font-[500] text-[16.5813px] leading-[20px] tracking-[-0.05em] text-[#959595]">
+          October
+        </p>
+        <p className="w-[76px] h-[20px] absolute top-[100px] left-[440px] font-[500] text-[16.5813px] leading-[20px] tracking-[-0.05em] text-[#959595]">
+          December
+        </p>
+        <hr className="w-[55.41px] h-[251.84px] absolute top-[-35px] left-[430px] border-black/[.3] rotate-90"></hr>
+        <h1 className="w-[217px] h-[35px] absolute top-[60px] left-[640px] font-[600] text-[29.3972px] leading-[35px] tracking-[-0.08em] text-[#48AF78]">
+          20 Rooms
+        </h1>
+        <p className="w-[160px] h-[20px] absolute top-[100px] left-[640px] font-[500] text-[16.9257px] leading-[20px] tracking-[-0.05em] text-[#959595]">
+          10 Singles, 10 doubles
+        </p>
+        <div className="w-[387px] h-[48px] absolute top-[150px] left-[10px] bg-[#FDF307] border-box border-[#FDF307] rounded-[24px_0px_0px_24px]">
+          <p className="w-[72px] h-[20px] absolute top-[15px] left-[30px] font-[500] text-[16.9257px] leading-[20px] tracking-[-0.07em] text-[#959595]">
+            Received
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
